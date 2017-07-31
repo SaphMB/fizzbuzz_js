81cb52150b408138df475aad6a2f5b343b9d76db
Alison Woodman <aliwoodman@gmail.com>
2017-07-31 16:48:31 +0100

    Divisble by 5 test passing

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -2,14 +2,20 @@ describe('Javabuzz', function() {
 
   var javabuzz;
 
+  beforeEach(function() {
+    javabuzz = new Javabuzz();
+  });
+
   describe('knows when a number is', function() {
 
     it('divisible by 3', function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(3)).toBe(true);
-
     });
 
+    it('divisible by 5', function() {
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+    });
   });
+
 
 });
